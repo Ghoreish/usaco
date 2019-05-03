42e182b14c740e6ad24d7810c6417a8c1cb1e4ea
ghoreish <sghoreish@gmail.com>
2019-05-03 22:45:46 +0430

    numtri solved

--- numtri/numtri4.py
+++ numtri/numtri4.py
@@ -3,26 +3,20 @@ ID: ali99gh1
 LANG: PYTHON3
 TASK: numtri
 """
+import random
 f=open("numtri.in","r")
 o=open("numtri.out","w")
 n=int(f.readline()[:-1])
 l=[]
 for i in range(n):
-    g=f.readline()[:-1].split()
-    k=[]
-    for j in g:
-        k.append(int(j))
-    l.append(eval(str(k)))
-k=set()
-def make(x,y,a):
-    if x<n-1 and y<n-1:
-        for i in [(x+1,y),(x+1,y+1)]:
-            a+=l[i[0]][i[1]]
-            make(i[0],i[1],a)
-            a-=l[i[0]][i[1]]
-    else:
-        k.add(a)
-make(0,0,l[0][0])
-o.write(str(max(k))+"\n")
+    a=f.readline()[:-1].split()
+    j=[]
+    for k in a:
+        j.append(int(k))
+    l.append(eval(str(j)))
+for i in range(n-2,-1,-1):
+    for j in range(len(l[i])):
+        l[i][j]+=max([l[i+1][j],l[i+1][j+1]])
+o.write(str(l[0][0])+"\n")
 f.close()
 o.close()
